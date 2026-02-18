affeededa07847a9c9fadf8dea103c087975b28e
Alex S <alex.silvan.92@gmail.com>
2026-02-18 16:37:49 +0000

    feat: Implementación del 🔐 Registro e inicio de sesión. (Funcionando)
    - Se añadió compatibilidad con JWT para la autenticación de usuarios en la API de Flask.
    - Se crearon nuevos endpoints para el registro e inicio de sesión de usuarios.
    - Se implementó la gestión de errores de expiración de tokens JWT y tokens no válidos.
    - Se actualizó el componente Navbar  para incluir la funcionalidad de login/logout.
    - Se crearon pages de Login y Signup con formularios básicos.
    - Se añadió el componente PrivateRoute para proteger las rutas que requieren autenticación.
    - Se integró la gestión del estado del usuario en el global reducer.
    - Se implementaron servicios para gestionar las solicitudes de API relacionadas con la autenticación.

--- src/front/pages/Layout.jsx
+++ src/front/pages/Layout.jsx
@@ -2,9 +2,26 @@ import { Outlet } from "react-router-dom/dist"
 import ScrollToTop from "../components/ScrollToTop"
 import { Navbar } from "../components/Navbar"
 import { Footer } from "../components/Footer"
+import useGlobalReducer from "../hooks/useGlobalReducer"
+import { getMeService } from "../services/authService"
+import { useEffect } from "react"
 
 // Base component that maintains the navbar and footer throughout the page and the scroll to top functionality.
 export const Layout = () => {
+    const { store, dispatch } = useGlobalReducer();
+
+    useEffect(() => {
+        if (!store.token) return;
+
+        getMeService(store.token).then(([data, error]) => {
+            if (error) {
+                dispatch({ type: "logout" });
+                return;
+            }
+            dispatch({ type: "set_user", payload: data });
+        });
+    }, []);
+
     return (
         <ScrollToTop>
             <Navbar />
